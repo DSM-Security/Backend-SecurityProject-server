6ed19d6f20dac0f8e1ceb4f8d9f3a4e6175b7d49
KoGyeongtae <kokt0203@naver.com>
2021-09-25 11:50:47 +0900

    EDIT - basic form

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "github.com/Backend-SecurityProject-server/server"
 
 func main() {
-
+	// start server
 	server.Start(8000)
 }
--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/Backend-SecurityProject-server/server/v1/auth"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -16,14 +17,14 @@ func Start(port int) {
 	api := app.Group("api")
 	v1R := api.Group("v1")
 
-	v1auth := v1R.Group("auth")
-	v1auth.Post("/login")
-	v1auth.Post("/signup")
+	v1auth := v1R.Group("auth", auth.Login)
+	v1auth.Post("/login", auth.Login)
+	v1auth.Post("/signup", auth.Login)
 
 	v1post := v1R.Group("post")
-	v1post.Get("/get")
-	v1post.Post("/post")
-	v1post.Delete("/delete")
+	v1post.Get("/get", auth.Login)
+	v1post.Post("/post", auth.Login)
+	v1post.Delete("/delete", auth.Login)
 
 	log.Fatal(app.Listen(":" + fmt.Sprint(port)))
 }
