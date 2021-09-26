0ca7a9f94a63532c90d3192342ea351b53fa6236
KoGyeongtae <kokt0203@naver.com>
2021-09-26 20:29:06 +0900

    EDIT - 일하기 싫다

--- server/server.go
+++ server/server.go
@@ -1,10 +1,12 @@
 package server
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/Backend-SecurityProject-server/server/v1/auth"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -12,12 +14,17 @@ import (
 func Start(port int) {
 	app := fiber.New()
 
+	db, err := sql.Open("mysql", "root:4451@tcp(127.0.0.1:3306)/security")
+	log.Print(err)
+
+	defer db.Close()
+
 	app.Use(cors.New())
 
 	api := app.Group("api")
 	v1R := api.Group("v1")
 
-	v1auth := v1R.Group("auth", auth.Login)
+	v1auth := v1R.Group("auth")
 	v1auth.Post("/login", auth.Login)
 	v1auth.Post("/signup", auth.Login)
 
