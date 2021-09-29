9f7e2a5bf4eab3ad41994b49b341b404e7558317
KoGyeongtae <kokt0203@naver.com>
2021-09-29 11:53:31 +0900

    EDIT - DB setting

--- server/db/db.go
+++ server/db/db.go
@@ -10,6 +10,7 @@ var err error
 
 func Start() {
 	db, err = sql.Open("mysql", "root:4451@tcp(127.0.0.1:3306)/security")
+	Migrate()
 	if err != nil {
 		log.Panic(err)
 	}
@@ -21,4 +22,8 @@ func GetDB() *sql.DB {
 
 func CloseDB() {
 	db.Close()
+}
+
+func Migrate() {
+	db.Query("CREATE TABLE user (id VARCHAR(20) PRIMARY KEY, password VARCHAR(255), nickname VARCHAR(30))")
 }
